1a10da5dfa6b4f04f573790091715bfc3177a0f7
AJ-Kulundu <jameskulundu@gmail.com>
2022-03-25 10:43:59 +0300

    Adding the head component for SEO

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,12 @@
 import "../styles/globals.css";
 import Layout from "../components/Layout";
 import Navbar from "../components/Navbar.js";
+import Header from '../components/Header';
 
 function MyApp({ Component, pageProps }) {
   return (
     <Layout>
+      <Header />
       <Navbar />
       <Component {...pageProps} />
     </Layout>
