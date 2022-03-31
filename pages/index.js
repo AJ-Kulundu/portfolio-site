e8a0649e6a15830228acec8fc837f63c2b3031b0
AJ-Kulundu <jameskulundu@gmail.com>
2022-03-31 14:06:39 +0300

    Ading the skills component to the home page

--- pages/index.js
+++ pages/index.js
@@ -3,10 +3,14 @@ import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import Landing from '../components/landing/Home'
 import Contact from '../components/Contact'
+import Skills from '../components/Skills'
+
+
 export default function Home() {
   return (
     <div>
    <Landing />
+   <Skills />
    <Contact />
    </div>
   )
