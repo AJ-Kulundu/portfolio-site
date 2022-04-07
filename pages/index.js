bab662ff8f7bdb0f2badd922e7868e1c16e8dea7
AJ-Kulundu <jameskulundu@gmail.com>
2022-04-07 12:02:05 +0300

    Adding environment variable from server side

--- pages/index.js
+++ pages/index.js
@@ -3,13 +3,29 @@ import Contact from "../components/Contact";
 import Skills from "../components/Skills";
 import Project from "../components/Project";
 
-export default function Home() {
+export default function Home({ user_id, service_id, template_id,user_email }) {
   return (
     <div>
       <Landing />
       <Project />
       <Skills />
-      <Contact />
+      <Contact
+        userID={user_id}
+        serviceID={service_id}
+        templateID={template_id}
+        userEmail={user_email}
+      />
     </div>
   );
 }
+
+export const getServerSideProps = async () => {
+  return {
+    props: {
+      user_id: process.env.EMAILJS_USERID,
+      template_id: process.env.EMAILJS_TEMPLATEID,
+      service_id: process.env.EMAILJS_SERVICEID,
+      user_email: process.env.MY_EMAIL
+    },
+  };
+};
