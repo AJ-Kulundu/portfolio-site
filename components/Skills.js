f27e6f2fcfc80dc7fd79d2414dcac290b4fba4d9
AJ-Kulundu <jameskulundu@gmail.com>
2022-07-02 15:07:07 +0300

    Adding the proper alignment to the skills component

--- components/Skills.js
+++ components/Skills.js
@@ -2,55 +2,67 @@ import React from "react";
 import {
   Flex,
   Heading,
-  Stack,
   Text,
   List,
   ListItem,
   ListIcon,
   SimpleGrid,
+  VStack,
 } from "@chakra-ui/react";
-import { FaReact,FaNodeJs,FaGitAlt,FaCloud,FaPython,FaDatabase } from "react-icons/fa";
-import { SiTypescript,SiJavascript } from 'react-icons/si';
+import {
+  FaReact,
+  FaNodeJs,
+  FaGitAlt,
+  FaCloud,
+  FaPython,
+  FaDatabase,
+} from "react-icons/fa";
+import { SiTypescript, SiJavascript } from "react-icons/si";
 const Skills = () => {
   return (
-    <Stack>
-      <Flex direction={"column"} p={8} px={16}>
+    <VStack spacing={5} p={8}>
+      <Flex justify={"flex-start"}  w="full">
         <Heading>Skills</Heading>
+      </Flex>
+      <Flex justify={"flex-start"}  w="full">
         <Text>
           Through my studies, I have gained a lot of expirience and
           understanding of computer science concepts and web development
-          concepts, which I use to provide solutions to real world problems.
+          concepts, which I use to provide solutions to real world problems. I
+          have gained the following skills:
         </Text>
+      </Flex>
+      <Flex  justify={"flex-start"} w="full">
         <List>
-          <SimpleGrid columns={{base:1,md:2,lg:4}} spacingY={4}>
-          <ListItem>
-            <ListIcon as={FaReact} /> React{" "}
-          </ListItem>
-          <ListItem>
-            <ListIcon as={FaNodeJs} /> NodeJS{" "}
-          </ListItem>
-          <ListItem>
-            <ListIcon as={FaGitAlt} /> Git{" "}
-          </ListItem>
-          <ListItem>
-            <ListIcon as={SiTypescript} /> TypeScript{" "}
-          </ListItem>
-          <ListItem>
-            <ListIcon as={FaPython} /> Python{" "}
-          </ListItem>
-          <ListItem>
-            <ListIcon as={SiJavascript} /> JavaScript{" "}
-          </ListItem>
-          <ListItem>
-            <ListIcon as={FaCloud} /> DevOps{" "}
-          </ListItem>
-          <ListItem>
-            <ListIcon as={FaDatabase} /> SQL & NoSQL{" "}
-          </ListItem>
+          <SimpleGrid columns={{ base: 1, md: 2, lg: 4 }} py={4} spacingX={{base:4, md:16, lg:24}} spacingY={{base:4,md:6,lg:8}} >
+            <ListItem>
+              <ListIcon as={FaReact} /> React{" "}
+            </ListItem>
+            <ListItem>
+              <ListIcon as={FaNodeJs} /> NodeJS{" "}
+            </ListItem>
+            <ListItem>
+              <ListIcon as={FaGitAlt} /> Git{" "}
+            </ListItem>
+            <ListItem>
+              <ListIcon as={SiTypescript} /> TypeScript{" "}
+            </ListItem>
+            <ListItem>
+              <ListIcon as={FaPython} /> Python{" "}
+            </ListItem>
+            <ListItem>
+              <ListIcon as={SiJavascript} /> JavaScript{" "}
+            </ListItem>
+            <ListItem>
+              <ListIcon as={FaCloud} /> DevOps{" "}
+            </ListItem>
+            <ListItem>
+              <ListIcon as={FaDatabase} /> SQL{" "}
+            </ListItem>
           </SimpleGrid>
         </List>
       </Flex>
-    </Stack>
+    </VStack>
   );
 };
 
