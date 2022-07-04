168f75d5026dc82211efa6972a50a5654f6f9289
AJ-Kulundu <jameskulundu@gmail.com>
2022-07-04 20:41:53 +0300

    Adjusting the skills padding for lg screens

--- components/Skills.js
+++ components/Skills.js
@@ -21,18 +21,18 @@ import { SiTypescript, SiJavascript } from "react-icons/si";
 const Skills = () => {
   return (
     <VStack spacing={5} p={8}>
-      <Flex justify={"flex-start"}  w="full">
+      <Flex justify={"flex-start"} px={{lg:10}} w="full">
         <Heading>Skills</Heading>
       </Flex>
-      <Flex justify={"flex-start"}  w="full">
+      <Flex justify={"flex-start"} px={{lg:10}} w="full">
         <Text>
-          Through my studies, I have gained a lot of expirience and
+          Through my studies, I have gained a lot of experience and
           understanding of computer science concepts and web development
           concepts, which I use to provide solutions to real world problems. I
           have gained the following skills:
         </Text>
       </Flex>
-      <Flex  justify={"flex-start"} w="full">
+      <Flex  justify={"flex-start"} px={{lg:10}} w="full">
         <List>
           <SimpleGrid columns={{ base: 1, md: 2, lg: 4 }} py={4} spacingX={{base:4, md:16, lg:24}} spacingY={{base:4,md:6,lg:8}} >
             <ListItem>
