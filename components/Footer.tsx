44e191f2df5dce06628a26a3c58fd7db65b51749
AJ-Kulundu <jameskulundu@gmail.com>
2022-08-13 19:32:22 +0300

    Formatting footer styles

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -11,29 +11,29 @@ const FooterLink = ({ href, children }: { href: string; children: string }) => {
 
 const Footer = () => {
   return (
-    <footer className="flex flex-col mx-auto mb-8 w-full max-w-2xl">
-        <hr className="border-2 mb-4 border-neutral-200 dark:border-neutral-800" />
-    <div className=" grid grid-cols-1 gap-y-4 md:grid-cols-3">
-      <div className="flex flex-col space-y-2 md:space-y-4">
-        <FooterLink href="/">Home</FooterLink>
-        <FooterLink href="/blog">Blog</FooterLink>
-        <FooterLink href="/tweets">Tweets</FooterLink>
+    <footer className="mx-auto mb-8 flex w-full max-w-2xl flex-col">
+      <hr className="mb-4 border-2 border-neutral-200 dark:border-neutral-800" />
+      <div className=" grid grid-cols-1 gap-y-4 md:grid-cols-3">
+        <div className="flex flex-col space-y-2 md:space-y-4">
+          <FooterLink href="/">Home</FooterLink>
+          <FooterLink href="/blog">Blog</FooterLink>
+          <FooterLink href="/tweets">Tweets</FooterLink>
+        </div>
+        <div className="flex flex-col space-y-2 md:space-y-4">
+          <FooterLink href="/">Home</FooterLink>
+          <FooterLink href="/">Home</FooterLink>
+          <FooterLink href="/">Home</FooterLink>
+        </div>
+        <div className="flex flex-col space-y-2 md:space-y-4">
+          <FooterLink href="/">Github</FooterLink>
+          <FooterLink href="/">Twitter</FooterLink>
+          <FooterLink href="/">LinkedIn</FooterLink>
+          <FooterLink href="/">Email</FooterLink>
+          <FooterLink href="/">Medium</FooterLink>
+        </div>
       </div>
-      <div className="flex flex-col space-y-2 md:space-y-4">
-        <FooterLink href="/">Home</FooterLink>
-        <FooterLink href="/">Home</FooterLink>
-        <FooterLink href="/">Home</FooterLink>
-      </div>
-      <div className="flex flex-col space-y-2 md:space-y-4">
-        <FooterLink href="/">Github</FooterLink>
-        <FooterLink href="/">Twitter</FooterLink>
-        <FooterLink href="/">LinkedIn</FooterLink>
-        <FooterLink href="/">Email</FooterLink>
-        <FooterLink href="/">Medium</FooterLink>
-      </div>
-    </div>
     </footer>
   )
 }
 
-export default Footer;
+export default Footer
