de544e4d6fae171ab63450810cdb717fe6dc5248
AJ-Kulundu <jameskulundu@gmail.com>
2022-08-18 13:07:07 +0300

    Linking the Blog card to its respective blog

--- components/BlogCard.tsx
+++ components/BlogCard.tsx
@@ -1,21 +1,26 @@
 import React from "react"
 import { Blog } from "contentlayer/generated"
 import { formattedDate } from "@libs/formattedDate"
+import Link from "next/link"
 
 const BlogCard = ({ blog }: { blog: Blog }) => {
-    console.log(blog.slug)
   return (
-    <div className="mx-auto flex flex-col items-start space-y-2 rounded-2xl bg-neutral-900/5 p-6  shadow-sm dark:bg-neutral-100/5">
-      <h1 className="text-lg font-semibold tracking-wide">{blog.title}</h1>
-      <div className="flex flex-row">
-        <span className="text-neutral-900/50 dark:text-neutral-100/50">
-          {formattedDate(blog.date)}
+    <Link href={blog.url} passHref>
+      <a className="mx-auto flex w-full flex-col items-start space-y-2 rounded-2xl bg-neutral-900/5 p-6 shadow-sm hover:bg-neutral-900/20 dark:bg-neutral-100/5 hover:dark:bg-neutral-100/20">
+        <h1 className="text-xl font-semibold tracking-wide">{blog.title}</h1>
+        <div className="flex flex-row">
+          <span className="text-neutral-900/50 dark:text-neutral-100/50">
+            {formattedDate(blog.date)}
+          </span>
+        </div>
+        <span className="whitespace-pre-wrap text-lg font-medium">
+          {blog.description}
         </span>
-      </div>
-      <span className="text-md whitespace-pre-wrap font-medium">
-        {blog.description}
-      </span>
-    </div>
+        <span className="text-neutral-900/50 dark:text-neutral-100/50">
+          {blog.readingTime.text}
+        </span>
+      </a>
+    </Link>
   )
 }
 
