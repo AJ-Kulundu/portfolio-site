4bb55751572210dbabaa8a29947eeef36af84547
AJ-Kulundu <jameskulundu@gmail.com>
2022-08-24 13:30:55 +0300

    Adding the views display to the BlogCard component

--- components/BlogCard.tsx
+++ components/BlogCard.tsx
@@ -1,17 +1,21 @@
 import React from "react"
 import { Blog } from "contentlayer/generated"
 import { formattedDate } from "@libs/formattedDate"
-import Link from "next/link"
+import Link from "next/link";
+import {useBlogViews} from '@libs/useBlogViews';
+import { Loading } from './Loading';
 
 const BlogCard = ({ blog }: { blog: Blog }) => {
+  const {views,isLoading,isError} = useBlogViews(blog.slug)
   return (
     <Link href={blog.url} passHref>
       <a className="mx-auto flex w-full flex-col items-start space-y-2 rounded-2xl bg-neutral-900/5 p-6 shadow-sm hover:bg-neutral-900/20 dark:bg-neutral-100/5 hover:dark:bg-neutral-100/20">
         <h1 className="text-xl font-semibold tracking-wide">{blog.title}</h1>
-        <div className="flex flex-row">
-          <span className="text-neutral-900/50 dark:text-neutral-100/50">
+        <div className="flex flex-row space-x-4 w-1/5 items-center">
+          <span className="text-neutral-900/50 dark:text-neutral-100/50 ">
             {formattedDate(blog.date)}
           </span>
+          {!isError || !isLoading ? <Loading/> : (<span className="text-neutral-900/50 dark:text-neutral-100/50">{views} views</span>)}
         </div>
         <span className="whitespace-pre-wrap text-lg font-medium">
           {blog.description}
