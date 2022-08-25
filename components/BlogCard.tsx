b0aaaf35d6708148ea109cc5dfdefef93fe35ec9
AJ-Kulundu <jameskulundu@gmail.com>
2022-08-25 23:14:11 +0300

    Adding likes display to the blog

--- components/BlogCard.tsx
+++ components/BlogCard.tsx
@@ -3,23 +3,36 @@ import { Blog } from "contentlayer/generated"
 import { formattedDate } from "@libs/formattedDate"
 import Link from "next/link"
 import { useBlogViews } from "@libs/useBlogViews"
+import { useBlogLikes } from "@libs/useBlogLikes"
 import { Loading } from "./Loading"
 
 const BlogCard = ({ blog }: { blog: Blog }) => {
-  const { views, isLoading, isError } = useBlogViews(blog.slug)
+  const { views, isLoading:viewsLoading, isError:viewsError } = useBlogViews(blog.slug)
+  const {
+    likes,
+    isError: likesError,
+    isLoading: likesLoading,
+  } = useBlogLikes(blog.slug)
   return (
     <Link href={blog.url} passHref>
       <a className="mx-auto flex w-full flex-col items-start space-y-2 rounded-2xl bg-neutral-900/5 p-6 shadow-sm hover:bg-neutral-900/20 dark:bg-neutral-100/5 hover:dark:bg-neutral-100/20">
         <h1 className="text-xl font-semibold tracking-wide">{blog.title}</h1>
-        <div className="flex w-1/5 flex-row items-center space-x-4">
+        <div className="flex flex-row items-center space-x-4">
           <span className="text-neutral-900/50 dark:text-neutral-100/50 ">
             {formattedDate(blog.date)}
           </span>
-          {isError || isLoading ? (
+          {viewsError || viewsLoading ? (
             <Loading />
           ) : (
             <span className="text-neutral-900/50 dark:text-neutral-100/50">
               {views} views
+            </span>
+          )}
+          {likesLoading || likesError ? (
+            <Loading />
+          ) : (
+            <span className="text-neutral-900/50 dark:text-neutral-100/50">
+              {likes} likes
             </span>
           )}
         </div>
--- components/PostMetrics.tsx
+++ components/PostMetrics.tsx
@@ -1,24 +1,42 @@
 import React, { useEffect } from "react"
 import { useBlogViews } from "@libs/useBlogViews"
+import { useBlogLikes } from "@libs/useBlogLikes"
 import { Loading } from "./Loading"
 
 const PostMetrics = ({ slug }: { slug: string }) => {
-  const { views, isError, isLoading, increment } = useBlogViews(slug, {
+  const {
+    views,
+    isError: viewsError,
+    isLoading: viewsLoading,
+    increment: viewsIncrement,
+  } = useBlogViews(slug, {
     revalidateOnMount: false,
   })
+  const {
+    likes,
+    isError: likesError,
+    isLoading: likesLoading,
+  } = useBlogLikes(slug, { revalidateOnMount: false })
 
-  useEffect(() => increment(), [])
+  useEffect(() => viewsIncrement(), [])
 
   return (
-    <>
-      {isError || isLoading ? (
+    <div className="space-x-4">
+      {viewsError || viewsLoading ? (
         <Loading />
       ) : (
         <span className="text-neutral-900/50 dark:text-neutral-100/50">
           {views} views
         </span>
       )}
-    </>
+      {likesLoading || likesError ? (
+        <Loading />
+      ) : (
+        <span className="text-neutral-900/50 dark:text-neutral-100/50">
+          {likes} likes
+        </span>
+      )}
+    </div>
   )
 }
 
