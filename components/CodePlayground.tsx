9e11b47ede3477c5b7b7988253af0c1c281afe27
AJ-Kulundu <jameskulundu@gmail.com>
2022-09-15 15:41:19 +0300

    Styling the code playground

--- components/CodePlayground.tsx
+++ components/CodePlayground.tsx
@@ -9,11 +9,13 @@ export function Playground({
 }) {
   return (
     <div className="mb-6 md:-mx-10">
-      <div className="flex items-center justify-center rounded-xl border border-black/40 bg-black/20 px-4 py-10 md:px-8 md:py-18">
+      <div className="md:py-18 flex items-center justify-center rounded-xl border dark:border-black/30 border-white/30 bg-black/20 dark:bg-white/20 px-4 py-10 md:px-8">
         {children}
       </div>
       {description ? (
-        <div className="mt-3 text-sm text-neutral-900/50 dark:text-neutral-100/50">{description}</div>
+        <div className="mt-3 text-sm text-neutral-900/50 dark:text-neutral-100/50">
+          {description}
+        </div>
       ) : null}
     </div>
   )
