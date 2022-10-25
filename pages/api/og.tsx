1f382e01e2d76bc19f0f96278f0dfb12be4d9926
AJ-Kulundu <jameskulundu@gmail.com>
2022-10-25 16:07:58 +0300

    Adding a custom font to the Open Graph Image

--- pages/api/og.tsx
+++ pages/api/og.tsx
@@ -1,40 +1,56 @@
-import { ImageResponse } from "@vercel/og"
-import {NextRequest} from "next/server"
+import { ImageResponse } from "@vercel/og";
+import { NextRequest } from "next/server";
 
 export const config = {
   runtime: "experimental-edge",
 }
 
-export default function handler(req:NextRequest) {
-  try{
-    const {searchParams} = new URL(req.url);
 
-    const hasTitle = searchParams.has('title');
-    const title = hasTitle? searchParams.get('title')?.slice(0,100) : "Developer & Writer";
-  return new ImageResponse(
-    (
-      <div
-        tw=" flex justify-start items-end w-full h-full p-10 mb-4 "
-      >
-        <img 
-        width="150"
-          height="150"
-          src={`https://github.com/AJ-Kulundu.png`}
-          style={{
-            borderRadius: 128,
-          }}/>
-        <h1 tw="text-5xl tracking-wide capitalize font-semibold ml-2">AJ Kulundu - {title}</h1>
-      </div>
-    ),
-    {
-      width: 1000,
-      height: 600,
-    },
-  )
-}catch(e: any){
-  console.log(`${e.message}`);
-  return new Response(`Failed to generate the image`, {
-    status: 500,
-  });
-}
+const font = fetch(new URL('../../assets/WorkSans-Regular.otf', import.meta.url)).then((res) => res.arrayBuffer(),);
+export default  async function handler(req: NextRequest) {
+  try {
+    const { searchParams } = new URL(req.url)
+    const fontData= await font
+    const hasTitle = searchParams.has("title")
+    const title = hasTitle
+      ? searchParams.get("title")?.slice(0, 100)
+      : "Developer & Writer"
+    return new ImageResponse(
+      (
+        <div style={{ fontSize:100, fontFamily:'"Work Sans"'}} tw=" flex flex-col items-center justify-center w-full h-full py-40 px-10 rounded-lg bg-white">
+          <img
+            alt="GitHub Avatar"
+            width="200"
+            height="200"
+            src={`https://github.com/AJ-Kulundu.png`}
+            style={{
+              borderRadius: 128,
+            }}
+          />
+          <h1 tw="text-5xl capitalize font-bold ml-2 text-center">
+            AJ Kulundu 
+          </h1>
+          <h1 tw="text-5xl tracking-wide font-semibold ml-2 text-center">
+            {title}
+          </h1>
+        </div>
+      ),
+      {
+        width: 1000,
+        height: 600,
+        fonts: [
+          {
+            name: 'Work Sans',
+            data: fontData,
+            style: 'normal',
+          },
+        ],
+      },
+    )
+  } catch (e: any) {
+    console.log(`${e.message}`)
+    return new Response(`Failed to generate the image`, {
+      status: 500,
+    })
+  }
 }
