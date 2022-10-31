891ceddf53f9ae87e491cc30c8f46d9120212fae
AJ-Kulundu <jameskulundu@gmail.com>
2022-10-31 10:37:59 +0300

    fixing react typical

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,17 +2,9 @@ import type { NextPage } from "next"
 import Container from "@components/Container"
 import Typical from "react-typical"
 
-export const getStaticProps = () => {
-  const name = "AJ Kulundu"
 
-  return{
-    props:{
-      name
-    }
-  }
-}
 
-const Home: NextPage<{name:string}> = ({name}) => {
+const Home: NextPage = () => {
    
    
   return (
@@ -20,7 +12,7 @@ const Home: NextPage<{name:string}> = ({name}) => {
       <div className="mx-auto mb-20 flex w-full max-w-2xl flex-col justify-start space-y-6 py-20 md:mb-8">
         <span className="flex justify-start text-xl">Hello, my name is</span>
         <span className="flex justify-start text-4xl font-bold tracking-wide">
-          {name}
+          AJ Kulundu
         </span>
         <span className="flex justify-start text-2xl ">
           I&apos;m a&nbsp;
