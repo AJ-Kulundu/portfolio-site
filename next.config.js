9542287f67f7c8844bb89af09e5ea00b3a4c5941
AJ-Kulundu <jameskulundu@gmail.com>
2022-11-22 19:22:28 +0300

    Configuring next js for v13

--- next.config.js
+++ next.config.js
@@ -4,7 +4,14 @@ const nextConfig = {
    reactStrictMode: true,
   images:{
     domains:["pbs.twimg.com"]
-  }
+  },
+  experimental:{
+    fontLoaders:[
+      { loader: '@next/font/google', options: { subsets: ['latin'] } },
+    ],
+    appDir:true,
+  },
+ 
 }
 
 module.exports = withContentlayer(nextConfig);
