8fa886cfc8f8a18668a254c6475b459266b35d85
AJ-Kulundu <jameskulundu@gmail.com>
2022-11-23 16:17:57 +0300

    Head tags for the home page

---
+++ app/head.tsx
@@ -0,0 +1,12 @@
+export default async function Head() {
+  return (
+    <>
+      <title>AJ Kulundu - Developer & Writer</title>
+      <meta property="og:url" content="ajkulundu.com" />
+      <meta property="og:image" content="https://ajkulundu.com/api/og" />
+      <meta property="og:title" content="AJ Kulundu - Developer & Writer" />
+      <meta property="og:type" content="website" />
+      <meta property="og:site_name" content="AJ Kulundu" />
+    </>
+  )
+}
