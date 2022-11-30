daa7b91d4ccb58e388b85f8e007f4b02783570c9
AJ-Kulundu <jameskulundu@gmail.com>
2022-11-30 14:36:16 +0300

    Formatting the tweet page

--- app/tweet/page.tsx
+++ app/tweet/page.tsx
@@ -1,27 +1,21 @@
-import React from 'react';
+import React from "react"
 import { queryTweets } from "@libs/twitter"
 import { Tweet } from "@components/Tweet"
 
-
-export default async function TweetPage(){
-    const tweetIds = [
-        "1520782141075902464",
-        "1522560136346099712",
-        "1002105652792066048",
-        "1471558914579722245",
-        "1002109380706250752",
-        "1346476915838709763",
-        "1297189305023107072",
-      ]
-    const tweets = await queryTweets(tweetIds);
-    return(
-    
-      <div className="mx-auto max-w-2xl space-y-5 py-6">
-      <h1
-        className={`text-3xl font-semibold`}
-      >
-        Tweets
-      </h1>
+export default async function TweetPage() {
+  const tweetIds = [
+    "1520782141075902464",
+    "1522560136346099712",
+    "1002105652792066048",
+    "1471558914579722245",
+    "1002109380706250752",
+    "1346476915838709763",
+    "1297189305023107072",
+  ]
+  const tweets = await queryTweets(tweetIds)
+  return (
+    <div className="mx-auto max-w-2xl space-y-5 py-6">
+      <h1 className={`text-3xl font-semibold`}>Tweets</h1>
       <p className={`font-medium`}>
         I use twitter sometimes and find tweets that make me think, give me
         inspiration or explain my ideas better. I created this page to share
@@ -33,6 +27,5 @@ export default async function TweetPage(){
         ))}
       </div>
     </div>
-    )
+  )
 }
-
