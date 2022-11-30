a564a86eba87228cfa7d93790ea8f2912a74a0ac
AJ-Kulundu <jameskulundu@gmail.com>
2022-11-30 14:20:37 +0300

    Updating the head of the tweet page

--- app/tweet/head.tsx
+++ app/tweet/head.tsx
@@ -2,11 +2,14 @@ export default async function Head() {
   return (
     <>
       <title>Tweets - AJ Kulundu</title>
-      <meta  property="og:url" content="ajkulundu.com/tweets"/>
-      <meta  property="og:image" content="https://ajkulundu.com/api/og?title=My Favourite Tweets"/>
-      <meta  property="og:title" content="AJ Kulundu - Developer & Writer"/>
-      <meta  property="og:type" content="website"/>
-      <meta  property="og:site_name" content="AJ Kulundu"/>
+      <meta property="og:url" content="ajkulundu.com/tweets" />
+      <meta
+        property="og:image"
+        content="https://ajkulundu.com/api/og?title=My Favourite Tweets"
+      />
+      <meta property="og:title" content="AJ Kulundu - Developer & Writer" />
+      <meta property="og:type" content="website" />
+      <meta property="og:site_name" content="AJ Kulundu" />
     </>
   )
 }
