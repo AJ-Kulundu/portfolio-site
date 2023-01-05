8f570e77657187a6c7218714901865b720db8a31
AJ-Kulundu <jameskulundu@gmail.com>
2023-01-05 16:17:25 +0300

    Improving the SEO of the site

--- app/head.tsx
+++ app/head.tsx
@@ -2,11 +2,28 @@ export default async function Head() {
   return (
     <>
       <title>AJ Kulundu - Developer & Writer</title>
+      <meta name="robots" content="follow, index" />
       <meta property="og:url" content="ajkulundu.com" />
       <meta property="og:image" content="https://ajkulundu.com/api/og" />
       <meta property="og:title" content="AJ Kulundu - Developer & Writer" />
       <meta property="og:type" content="website" />
       <meta property="og:site_name" content="AJ Kulundu" />
+      <meta
+        property="og:description"
+        content="Fullstack web developer and writer"
+      />
+      <meta name="twitter:card" content="summary_large_image" />
+      <meta name="twitter:site" content="@ajkulundu" />
+      <meta name="twitter:title" content="AJ Kulundu - Developer & Writer" />
+      <meta
+        name="twitter:description"
+        content="Fullstack web developer and writer"
+      />
+      <meta name="twitter:image" content="https://ajkulundu.com/api/og" />
+      <meta
+        name="ahrefs-site-verification"
+        content="bfc8ab9a9bb8174e380b00473778fc96c5f89a65cbe63eb875172efa91536fea"
+      />
     </>
   )
 }
