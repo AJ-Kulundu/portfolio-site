3a4818f64eaf058193c24e12cc209e716e750878
AJ-Kulundu <jameskulundu@gmail.com>
2023-01-23 20:52:50 +0300

    Tweets page loading screen

---
+++ app/tweets/loading.tsx
@@ -0,0 +1,63 @@
+export default function Loading() {
+  return (
+    <div className="mx-auto max-w-2xl space-y-5 py-6 px-5">
+      <h1 className={`text-3xl font-semibold`}>Tweets</h1>
+      <p className={`text-lg font-medium`}>
+        I use twitter sometimes and find tweets that make me think, give me
+        inspiration or explain my ideas better. I created this page to share
+        some of my favourite tweets.
+      </p>
+      <div className="mt-4 space-y-10">
+        {/*  */}
+        <div className=" mx-auto min-h-[25%] w-full rounded-2xl bg-neutral-900/5 p-5 shadow-md">
+          <div className="mb-2 flex space-x-4 items-center">
+            <div className="flex h-10 w-10 animate-pulse items-center rounded-full bg-gray-900/10"></div>
+            <div className="h-6 w-3/5 animate-pulse rounded-2xl bg-gray-900/10"></div>
+          </div>
+          <div className="space-y-4">
+            <div className="h-4 w-4/5 animate-pulse rounded-lg bg-gray-900/10"></div>
+            <div className="h-4 w-4/5 animate-pulse rounded-lg bg-gray-900/10"></div>
+            <div className="h-4 w-2/5 animate-pulse rounded-lg bg-gray-900/10"></div>
+          </div>
+        </div>
+        {/*  */}
+        <div className=" mx-auto min-h-[25%] w-full rounded-2xl bg-neutral-900/5 p-5 shadow-md">
+          <div className="mb-2 flex  space-x-4 items-center">
+            <div className="flex h-10 w-10 animate-pulse items-center rounded-full bg-gray-900/10"></div>
+            <div className="h-6 w-3/5 animate-pulse rounded-2xl bg-gray-900/10"></div>
+          </div>
+          <div className="space-y-4">
+            <div className="h-4 w-4/5 animate-pulse rounded-lg bg-gray-900/10"></div>
+            <div className="h-4 w-4/5 animate-pulse rounded-lg bg-gray-900/10"></div>
+            <div className="h-4 w-2/5 animate-pulse rounded-lg bg-gray-900/10"></div>
+          </div>
+        </div>
+        {/*  */}
+        <div className=" mx-auto min-h-[25%] w-full rounded-2xl bg-neutral-900/5 p-5 shadow-md">
+          <div className="mb-2 flex space-x-4 items-center">
+            <div className="flex h-10 w-10 animate-pulse items-center rounded-full bg-gray-900/10"></div>
+            <div className="h-6 w-3/5 animate-pulse rounded-2xl bg-gray-900/10"></div>
+          </div>
+          <div className="space-y-4">
+            <div className="h-4 w-4/5 animate-pulse rounded-lg bg-gray-900/10"></div>
+            <div className="h-4 w-4/5 animate-pulse rounded-lg bg-gray-900/10"></div>
+            <div className="h-4 w-2/5 animate-pulse rounded-lg bg-gray-900/10"></div>
+          </div>
+        </div>
+        {/*  */}
+        <div className=" mx-auto min-h-[25%] w-full rounded-2xl bg-neutral-900/5 p-5 shadow-md">
+          <div className="mb-2 flex space-x-4 items-center">
+            <div className="flex h-10 w-10 animate-pulse items-center rounded-full bg-gray-900/10"></div>
+            <div className="h-6 w-3/5 animate-pulse rounded-2xl bg-gray-900/10"></div>
+          </div>
+          <div className="space-y-4">
+            <div className="h-4 w-4/5 animate-pulse rounded-lg bg-gray-900/10"></div>
+            <div className="h-4 w-4/5 animate-pulse rounded-lg bg-gray-900/10"></div>
+            <div className="h-4 w-2/5 animate-pulse rounded-lg bg-gray-900/10"></div>
+          </div>
+        </div>
+        {/*  */}
+      </div>
+    </div>
+  )
+}
