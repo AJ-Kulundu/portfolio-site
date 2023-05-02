e7054cf98e3eb15b7f2840b91961e95dbc8f315d
AJ-Kulundu <jameskulundu@gmail.com>
2023-05-02 21:21:05 +0300

    Creating the loading state for the blogs page

---
+++ app/blog/loading.tsx
@@ -0,0 +1,53 @@
+export default function Loading() {
+  return (
+    <div className="mx-auto flex max-w-2xl flex-col space-y-5 py-10">
+      <h1 className="px-5 text-3xl font-bold tracking-wide">Blog Page</h1>
+      {/*  */}
+      <div className="flex flex-col justify-start space-y-2 rounded-xl p-5 shadow-md">
+        <div className="h-7 w-full animate-pulse rounded-lg bg-gray-900/10"></div>
+        <div className="flex flex-row items-center space-x-4 ">
+          <div className="h-4 w-1/12 animate-pulse rounded-lg bg-gray-900/10"></div>
+          <div className="font-light">&middot;</div>
+          <div className="h-4 w-1/12 animate-pulse rounded-lg bg-gray-900/10"></div>
+          <div className="font-light">&middot;</div>
+          <div className="h-4 w-1/12 animate-pulse rounded-lg bg-gray-900/10"></div>
+        </div>
+        <div className="h-5 w-full animate-pulse rounded-lg bg-gray-900/10"></div>
+        <div className="h-5 w-full animate-pulse rounded-lg bg-gray-900/10"></div>
+        <div className="h-5 w-3/5 animate-pulse rounded-lg bg-gray-900/10"></div>
+        <div className="h-4 w-1/6 animate-pulse rounded-lg bg-gray-900/10"></div>
+      </div>
+      {/*  */}
+      <div className="flex flex-col justify-start space-y-2 rounded-xl p-5 shadow-md">
+        <div className="h-7 w-full animate-pulse rounded-lg bg-gray-900/10"></div>
+        <div className="flex flex-row items-center space-x-4 ">
+          <div className="h-4 w-1/12 animate-pulse rounded-lg bg-gray-900/10"></div>
+          <div className="font-light">&middot;</div>
+          <div className="h-4 w-1/12 animate-pulse rounded-lg bg-gray-900/10"></div>
+          <div className="font-light">&middot;</div>
+          <div className="h-4 w-1/12 animate-pulse rounded-lg bg-gray-900/10"></div>
+        </div>
+        <div className="h-5 w-full animate-pulse rounded-lg bg-gray-900/10"></div>
+        <div className="h-5 w-full animate-pulse rounded-lg bg-gray-900/10"></div>
+        <div className="h-5 w-3/5 animate-pulse rounded-lg bg-gray-900/10"></div>
+        <div className="h-4 w-1/6 animate-pulse rounded-lg bg-gray-900/10"></div>
+      </div>
+      {/*  */}
+      <div className="flex flex-col justify-start space-y-2 rounded-xl p-5 shadow-md">
+        <div className="h-7 w-full animate-pulse rounded-lg bg-gray-900/10"></div>
+        <div className="flex flex-row items-center space-x-4 ">
+          <div className="h-4 w-1/12 animate-pulse rounded-lg bg-gray-900/10"></div>
+          <div className="font-light">&middot;</div>
+          <div className="h-4 w-1/12 animate-pulse rounded-lg bg-gray-900/10"></div>
+          <div className="font-light">&middot;</div>
+          <div className="h-4 w-1/12 animate-pulse rounded-lg bg-gray-900/10"></div>
+        </div>
+        <div className="h-5 w-full animate-pulse rounded-lg bg-gray-900/10"></div>
+        <div className="h-5 w-full animate-pulse rounded-lg bg-gray-900/10"></div>
+        <div className="h-5 w-3/5 animate-pulse rounded-lg bg-gray-900/10"></div>
+        <div className="h-4 w-1/6 animate-pulse rounded-lg bg-gray-900/10"></div>
+      </div>
+      {/*  */}
+    </div>
+  )
+}
