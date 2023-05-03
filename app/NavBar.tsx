e31ade8cf3040977509663b6d858968305d4d17f
AJ-Kulundu <jameskulundu@gmail.com>
2023-05-03 21:35:44 +0300

    Adding theme toggle button to the nav bar

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -1,12 +1,16 @@
 import NavLink from "./NavLink"
+import { ThemeToggle } from "@components/ThemeToggle";
 
 export default function NavBar() {
   return (
     <nav className=" sticky mx-auto flex w-full max-w-2xl flex-row items-center justify-start px-5 ">
-      <div className="flex flex-row items-center gap-x-4">
+      <div className="flex flex-row items-center justify-between w-full">
+        <div className="flex flex-row items-center space-x-4">
         <NavLink href="/">Home</NavLink>
         <NavLink href="/blog">Blog</NavLink>
         <NavLink href="/tweets">Tweets</NavLink>
+        </div>
+        <ThemeToggle />
       </div>
     </nav>
   )
