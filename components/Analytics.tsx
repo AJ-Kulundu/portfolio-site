b481fc6deadca71bb6720b00e55bfbde1d096cf3
AJ-Kulundu <jameskulundu@gmail.com>
2023-05-03 22:11:32 +0300

    Fixing the analytics component

--- components/Analytics.tsx
+++ components/Analytics.tsx
@@ -1,10 +1,7 @@
 "use client"
 
 import { Analytics as VercelAnalytics } from "@vercel/analytics/react"
-import type { ComponentProps } from "react"
 
-type AnalyticsProps = ComponentProps<typeof VercelAnalytics>
-
-export default function Analytics(props: AnalyticsProps) {
-  return <VercelAnalytics {...props} />
+export default function Analytics() {
+  return <VercelAnalytics  />
 }
